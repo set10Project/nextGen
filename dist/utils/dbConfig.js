b9e0fcfd23b0b39459d4abecfdf1b7b3b2a52d37
PetxCode <brighterdayscodelab@gmail.com>
2024-09-29 09:30:08 +0100

    db added

--- dist/utils/dbConfig.js
+++ dist/utils/dbConfig.js
@@ -11,7 +11,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbConfig = void 0;
 const mongoose_1 = require("mongoose");
-const url = "mongodb://127.0.0.1:27017/practiceDB";
+// const url: string = "mongodb://127.0.0.1:27017/practiceDB";
+const url = "mongodb+srv://skillscapeofficier:skillscapeofficier@cluster0.0dubq.mongodb.net/nextGenDB?retryWrites=true&w=majority&appName=Cluster0";
 const dbConfig = () => __awaiter(void 0, void 0, void 0, function* () {
     yield (0, mongoose_1.connect)(url)
         .then(() => {
